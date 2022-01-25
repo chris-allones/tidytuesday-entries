334aa822c809b0f79dfdfba958583e9bce23c436
chris-allones <christopher.allones@gmail.com>
2022-01-25 12:07:38 +0700

    week 4 tidytuesday

---
+++ 2022/w4_20220125/tt_w4_20220125.R
@@ -0,0 +1,18 @@
+# TidyTuesday Week 4: Board Games
+
+# Libraries
+
+
+# Reading data and wrangling
+
+# ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-25/ratings.csv')
+# details <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-25/details.csv')
+
+tuesdata <- tidytuesdayR::tt_load('2022-01-25')
+
+
+
+# Fonts
+
+
+# Plot
